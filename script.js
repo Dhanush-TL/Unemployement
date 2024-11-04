702a8088f2c66bb652df937dd1cffd592915593f
Dhanush-TL <147185807+Dhanush-TL@users.noreply.github.com>
2024-11-04 20:07:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('search-bar').addEventListener('input', function() {
+    // Implement search functionality here
+    console.log('Searching:', this.value);
+});
+
+document.getElementById('job-search-bar').addEventListener('input', function() {
+    // Implement job search functionality here
+    console.log('Job search:', this.value)
+});
